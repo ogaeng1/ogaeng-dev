9803d4210ed306b4a589be15036b481ba4a911ef
OH <rudals2148@naver.com>
2024-09-04 02:41:52 +0900

    feat: 소개 페이지

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <section className="px-5">
+      <article className="mx-auto max-w-[672px]"></article>
+    </section>
+  );
+};
+
+export default AboutPage;
