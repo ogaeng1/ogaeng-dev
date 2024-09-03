4942432b3ef44ddf3f30f378a81ced2a4324ae93
OH <rudals2148@naver.com>
2024-09-04 02:40:21 +0900

    chore: 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
@@ -15,10 +16,23 @@ const config: Config = {
       },
       boxShadow: {
         headerShadow: "rgb(0 0 0 / 8%) 0px 2.5px 1px",
+        darkShadow: "rgb(128 128 128 / 8%) 0px 2.5px 1px",
+      },
+      keyframes: {
+        underline: {
+          "0%": { width: "0%" },
+          "100%": { width: "100%" },
+        },
       },
       colors: {
         card: "#f0f0f0",
         lightTags: "#7eabcf",
+        darkTags: "#2196F3",
+        darkBg: "#1E1E1E",
+        darkLine: "#2196F3",
+      },
+      animation: {
+        underline: "underline 0.3s linear",
       },
     },
   },
