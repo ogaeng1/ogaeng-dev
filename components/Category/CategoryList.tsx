b48380701d9fdb85fd055db44923826459c9b6de
OH <rudals2148@naver.com>
2024-09-04 02:42:51 +0900

    feat: 카테고리

---
+++ components/Category/CategoryList.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { PostProps } from "../Post/post.types";
+
+const CategoryList = ({ post }: PostProps) => {
+  const path = useSearchParams();
+  const query = path.get("category");
+  const allTags = [
+    "All",
+    ...post
+      .flatMap((post) => post.metadata.tags)
+      .filter((tag, idx, self) => self.indexOf(tag) === idx),
+  ];
+
+  return (
+    <aside className="mx-auto max-w-[672px] flex gap-3 overflow-x-auto overflow-y-hidden">
+      {allTags.map((tag) => (
+        <Link
+          key={tag}
+          href={tag === "All" ? "/" : `/?category=${tag}`}
+          className="mb-3"
+        >
+          <div
+            className={`w-full text-md border rounded-md h-[36px] flex items-center gap-2 px-3 text-center ${
+              query === tag || (query === null && tag === "All")
+                ? "bg-darkTags text-white"
+                : ""
+            }`}
+          >
+            {tag}
+          </div>
+        </Link>
+      ))}
+    </aside>
+  );
+};
+
+export default CategoryList;
